c603dd8212c1df96e9bf295e021f2b2f45e4a953
dokimiki <57443812+dokimiki@users.noreply.github.com>
2025-07-23 21:55:23 +0900

    エラー解消

--- src/app/extensions/auth/page.tsx
+++ src/app/extensions/auth/page.tsx
@@ -1,6 +1,7 @@
 "use client";
 import { signInWithPopup, GoogleAuthProvider, getAuth } from "firebase/auth";
 import { initializeApp } from "firebase/app";
+import { useEffect } from "react";
 
 const firebaseConfig = {
     apiKey: "AIzaSyDjspgb04eUJRlNvkyGfQN0Vuug9OW5GsM",
@@ -14,32 +15,34 @@ const firebaseConfig = {
 };
 
 export default function AuthPage() {
-    initializeApp(firebaseConfig);
-    const auth = getAuth();
+    useEffect(() => {
+        initializeApp(firebaseConfig);
+        const auth = getAuth();
 
-    // This code runs inside of an iframe in the extension's offscreen document.
-    // This gives you a reference to the parent frame, i.e. the offscreen document.
-    // You will need this to assign the targetOrigin for postMessage.
-    const PARENT_FRAME = document.location.ancestorOrigins[0];
+        // This code runs inside of an iframe in the extension's offscreen document.
+        // This gives you a reference to the parent frame, i.e. the offscreen document.
+        // You will need this to assign the targetOrigin for postMessage.
+        const PARENT_FRAME = document.location.ancestorOrigins[0];
 
-    // This demo uses the Google auth provider, but any supported provider works.
-    // Make sure that you enable any provider you want to use in the Firebase Console.
-    // https://console.firebase.google.com/project/_/authentication/providers
-    const PROVIDER = new GoogleAuthProvider();
+        // This demo uses the Google auth provider, but any supported provider works.
+        // Make sure that you enable any provider you want to use in the Firebase Console.
+        // https://console.firebase.google.com/project/_/authentication/providers
+        const PROVIDER = new GoogleAuthProvider();
 
-    function sendResponse(result: object) {
-        globalThis.parent.self.postMessage(JSON.stringify(result), PARENT_FRAME);
-    }
-
-    globalThis.addEventListener("message", function ({ data }) {
-        if (data.initAuth) {
-            // Opens the Google sign-in page in a popup, inside of an iframe in the
-            // extension's offscreen document.
-            // To centralize logic, all respones are forwarded to the parent frame,
-            // which goes on to forward them to the extension's service worker.
-            signInWithPopup(auth, PROVIDER).then(sendResponse).catch(sendResponse);
+        function sendResponse(result: object) {
+            globalThis.parent.self.postMessage(JSON.stringify(result), PARENT_FRAME);
         }
-    });
+
+        globalThis.addEventListener("message", function ({ data }) {
+            if (data.initAuth) {
+                // Opens the Google sign-in page in a popup, inside of an iframe in the
+                // extension's offscreen document.
+                // To centralize logic, all respones are forwarded to the parent frame,
+                // which goes on to forward them to the extension's service worker.
+                signInWithPopup(auth, PROVIDER).then(sendResponse).catch(sendResponse);
+            }
+        });
+    }, []);
 
     return (
         <>
