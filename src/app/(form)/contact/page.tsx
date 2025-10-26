a86704dafafff71df1b45b17a98d1a5eaed88966
dokimiki <57443812+dokimiki@users.noreply.github.com>
2025-10-26 16:38:02 +0900

    ページ遷移

--- src/app/(form)/contact/page.tsx
+++ src/app/(form)/contact/page.tsx
@@ -1,17 +1,7 @@
-import { useEffect } from "react";
+import { redirect } from "next/navigation";
 
 export default function ContactPage() {
     const formURL = "https://docs.google.com/forms/d/e/1FAIpQLScnnbnSoIpNizG1gU_pDVuDq9GifE1EeevYd3-n4Uy1fGAQiw/viewform";
 
-    useEffect(() => {
-        location.href = formURL;
-    }, []);
-
-    return (
-        <div>
-            <a href={formURL} target="_blank" rel="noopener noreferrer">
-                お問い合わせフォーム
-            </a>
-        </div>
-    );
+    redirect(formURL);
 }
--- src/app/(form)/feedback/page.tsx
+++ src/app/(form)/feedback/page.tsx
@@ -1,17 +1,7 @@
-import { useEffect } from "react";
+import { redirect } from "next/navigation";
 
 export default function FeedbackPage() {
     const formURL = "https://docs.google.com/forms/d/e/1FAIpQLSeu7t-Z3PgwvG0aKY4XCSz160FyjE03SNUyu7Qhq11F_2Z_Dg/viewform";
 
-    useEffect(() => {
-        location.href = formURL;
-    }, []);
-
-    return (
-        <div>
-            <a href={formURL} target="_blank" rel="noopener noreferrer">
-                フィードバックフォーム
-            </a>
-        </div>
-    );
+    redirect(formURL);
 }
